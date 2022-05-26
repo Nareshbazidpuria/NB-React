af763f7a94fed44475777965d0ed9ea4b87965b1
Naresh <nareshbazidpuria.com>
2022-05-26 16:27:54 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './components/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home key='home' />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import logo from '../Favicon.png';
+
+const Navbar = () => {
+    let location = useLocation();
+    return (
+        <div>
+            <nav className="navbar-1" id="navbar-1">
+                <img className="nls-1" id="logo-1" src={logo} alt="Naresh Bazidpuria" /><span id="logoname-1"
+                    className="logoname-1 nlns-1">&nbsp;&nbsp;Naresh Bazidpuria&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                <ul>
+                    <li><Link className={`navOpt ${location.pathname === "/" ? 'active-1':''}`} to="/">Home</Link></li>
+                    <li><Link className={`navOpt ${location.pathname === "/About" ? 'active-1':''}`} to="/About">About</Link></li>
+                    <li><Link className={`navOpt ${location.pathname === "/Projects" ? 'active-1':''}`} to="/Projects">Projects</Link></li>
+                    <li><Link className={`navOpt ${location.pathname === "/ContactUs" ? 'active-1':''}`} to="/ContactUs">Contact Us</Link></li>
+
+                </ul>
+                <div className="icons-1">
+                    <a href="https://www.instagram.com/naresh_bazidpuria" target="_blank" rel="noreferrer"><i className="fa fa-instagram" style={{ "color": "#ff01c9" }}></i></a>
+                    <a href="https://www.facebook.com/nareshbazidpuria" target="_blank" rel="noreferrer"><i className="fa fa-facebook" style={{ "color": "blue" }}
+                        alt="facebook"></i></a>
+                    <a href="http://api.whatsapp.com/send?phone=+917837625927&text=How an I help you ?" target="_blank" rel="noreferrer"><i
+                        className="fa fa-whatsapp" style={{ "color": "#39b832" }}></i></a>
+                    <a href="https://twitter.com/nareshk84936106" target="_blank" rel="noreferrer"><i className="fa fa-twitter" style={{ "color": "#1c96e9" }}></i></a>
+                    <a href="https://www.youtube.com/c/NareshBazidpuria" target="_blank" rel="noreferrer"><i className="fa fa-youtube" style={{ "color": "red" }}></i></a>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
