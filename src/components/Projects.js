9a1a23ef694999148d4c5ebaabe08a78dbd44afa
Naresh <nareshbazidpuria.com>
2022-12-06 00:56:04 +0530

    removing projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,7 +8,7 @@ const Projects = (props) => {
                 <img id='nChatLogo' src={imgSrc} alt='' />
                 <div>
                     <div className='mph1'>{name}</div>
-                    <p style={{'width':'20rem','margin':'0 40px','fontSize': '20px'}}>{content} <br /><a className='goBtn' href={url} target='_blank' rel="noreferrer" >Go</a></p>
+                    <p style={{'width':'20rem','margin':'0 40px','fontSize': '20px'}}>{content+ '( under maintenance !)'} <br /><a className='goBtn' href={url} target='_blank' rel="noreferrer" >Temporary Unavailable</a></p>
                 </div>
             </div>
         </div>
